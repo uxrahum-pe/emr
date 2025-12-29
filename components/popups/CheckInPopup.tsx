c3bcd40b7adcbb0e4193b5d0d38982b29dc23ed1
uxmason <uxmason@gmail.com>
2025-12-29 18:04:22 +0900

    Update CheckInPopup and styles

--- components/popups/CheckInPopup.tsx
+++ components/popups/CheckInPopup.tsx
@@ -57,27 +57,283 @@ export default function CheckInPopup({ onClose }: CheckInPopupProps) {
         </div>
       </PopupSectionBox>
       <PopupSectionBox x={390} y={190} width={520} height={580}>
-        <div className="C230">
+        <div className="C233">
           <div className="C231">
             <p className="T098">Vital 고객 이력</p>
+          </div>
+          <div className="C234"></div>
+          <div className="C232">
+            <div className="C076">
+              <div className="C235">
+                <p className="T036">
+                  <span className="isUnit">25.</span>02.17
+                  <br></br>
+                  <span className="isUnit">AM</span> 10:30
+                </p>
+                <div className="C237">
+                  <p className="T014 isOldbie">
+                    2<span className="isUnit">기</span>
+                  </p>
+                </div>
+                <div className="C088">
+                  <p className="T099">
+                    김민수<span className="isUnit">과장</span>
+                  </p>
+                  <p className="T038">(kms002)</p>
+                </div>
+              </div>
+              <div className="C236">
+                <div className="C080">
+                  <div className="C081 styleSheet isIcon isMini isChecked"></div>
+                </div>
+                <div className="C082 isMale isGap"></div>
+              </div>
+              <div className="C238">
+                <div className="C239">
+                  <div className="C240">
+                    <p className="T100">키</p>
+                    <p className="T101">
+                      175<span className="isUnit">cm</span>
+                    </p>
+                  </div>
+                  <div className="C240">
+                    <p className="T100">B.P</p>
+                    <p className="T101">
+                      132<span className="isUnit">/</span>88
+                    </p>
+                  </div>
+                  <div className="C240">
+                    <p className="T100">R</p>
+                    <p className="T101">20</p>
+                  </div>
+                  <div className="C240">
+                    <p className="T100">몸무게</p>
+                    <p className="T101">
+                      62.5<span className="isUnit">kg (</span>
+                      <span className="isMagenta isValue">-0.2</span>
+                      <span className="isUnit">)</span>
+                    </p>
+                  </div>
+                </div>
+                <div className="C239">
+                  <div className="C240">
+                    <p className="T100">P.R</p>
+                    <p className="T101">69</p>
+                  </div>
+                  <div className="C240">
+                    <p className="T100">BT</p>
+                    <p className="T101">123</p>
+                  </div>
+                  <div className="C240">
+                    <p className="T100">B.T</p>
+                    <p className="T101">
+                      35.6<span className="isUnit"> ℃</span>
+                    </p>
+                  </div>
+                  <div className="C240">
+                    <p className="T100">간호사 확인</p>
+                    <div className="C214">
+                      <div className="C241"></div>
+                      <p className="T089">
+                        이미영
+                        <span className="isUnit">간호사</span>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="C076">
+              <div className="C235">
+                <p className="T036">
+                  <span className="isUnit">25.</span>02.17
+                  <br></br>
+                  <span className="isUnit">AM</span> 10:30
+                </p>
+                <div className="C237">
+                  <p className="T014 isOldbie">
+                    2<span className="isUnit">기</span>
+                  </p>
+                </div>
+                <div className="C088">
+                  <p className="T099">
+                    김민수<span className="isUnit">과장</span>
+                  </p>
+                  <p className="T038">(kms002)</p>
+                </div>
+              </div>
+              <div className="C236">
+                <div className="C080">
+                  <div className="C081 styleSheet isIcon isMini isChecked"></div>
+                </div>
+                <div className="C082 isMale isGap"></div>
+              </div>
+              <div className="C238">
+                <div className="C239">
+                  <div className="C240">
+                    <p className="T100">키</p>
+                    <p className="T101">
+                      175<span className="isUnit">cm</span>
+                    </p>
+                  </div>
+                  <div className="C240">
+                    <p className="T100">B.P</p>
+                    <p className="T101">
+                      132<span className="isUnit">/</span>88
+                    </p>
+                  </div>
+                  <div className="C240">
+                    <p className="T100">R</p>
+                    <p className="T101">20</p>
+                  </div>
+                  <div className="C240">
+                    <p className="T100">몸무게</p>
+                    <p className="T101">
+                      62.5<span className="isUnit">kg (</span>
+                      <span className="isMagenta isValue">-0.2</span>
+                      <span className="isUnit">)</span>
+                    </p>
+                  </div>
+                </div>
+                <div className="C239">
+                  <div className="C240">
+                    <p className="T100">P.R</p>
+                    <p className="T101">69</p>
+                  </div>
+                  <div className="C240">
+                    <p className="T100">BT</p>
+                    <p className="T101">123</p>
+                  </div>
+                  <div className="C240">
+                    <p className="T100">B.T</p>
+                    <p className="T101">
+                      35.6<span className="isUnit"> ℃</span>
+                    </p>
+                  </div>
+                  <div className="C240">
+                    <p className="T100">간호사 확인</p>
+                    <div className="C214">
+                      <div className="C241"></div>
+                      <p className="T089">
+                        이미영
+                        <span className="isUnit">간호사</span>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="C076">
+              <div className="C235">
+                <p className="T036">
+                  <span className="isUnit">25.</span>02.17
+                  <br></br>
+                  <span className="isUnit">AM</span> 10:30
+                </p>
+                <div className="C237">
+                  <p className="T014 isOldbie">
+                    2<span className="isUnit">기</span>
+                  </p>
+                </div>
+                <div className="C088">
+                  <p className="T099">
+                    김민수<span className="isUnit">과장</span>
+                  </p>
+                  <p className="T038">(kms002)</p>
+                </div>
+              </div>
+              <div className="C236">
+                <div className="C080">
+                  <div className="C081 styleSheet isIcon isMini isChecked"></div>
+                </div>
+                <div className="C082 isMale isGap"></div>
+              </div>
+              <div className="C238">
+                <div className="C239">
+                  <div className="C240">
+                    <p className="T100">키</p>
+                    <p className="T101">
+                      175<span className="isUnit">cm</span>
+                    </p>
+                  </div>
+                  <div className="C240">
+                    <p className="T100">B.P</p>
+                    <p className="T101">
+                      132<span className="isUnit">/</span>88
+                    </p>
+                  </div>
+                  <div className="C240">
+                    <p className="T100">R</p>
+                    <p className="T101">20</p>
+                  </div>
+                  <div className="C240">
+                    <p className="T100">몸무게</p>
+                    <p className="T101">
+                      62.5<span className="isUnit">kg (</span>
+                      <span className="isBlue isValue">+0.2</span>
+                      <span className="isUnit">)</span>
+                    </p>
+                  </div>
+                </div>
+                <div className="C239">
+                  <div className="C240">
+                    <p className="T100">P.R</p>
+                    <p className="T101">69</p>
+                  </div>
+                  <div className="C240">
+                    <p className="T100">BT</p>
+                    <p className="T101">123</p>
+                  </div>
+                  <div className="C240">
+                    <p className="T100">B.T</p>
+                    <p className="T101">
+                      35.6<span className="isUnit"> ℃</span>
+                    </p>
+                  </div>
+                  <div className="C240">
+                    <p className="T100">간호사 확인</p>
+                    <div className="C214">
+                      <div className="C241"></div>
+                      <p className="T089">
+                        이미영
+                        <span className="isUnit">간호사</span>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </PopupSectionBox>
+      <PopupSectionBox x={390} y={710} width={520} height={260}>
+        <div className="C233"></div>
+      </PopupSectionBox>
+      <PopupSectionBox x={390} y={990} width={520} height={140}>
+        <div className="C233"></div>
+      </PopupSectionBox>
+      <PopupSectionBox x={930} y={190} width={600} height={240}>
+        <div className="C233">
+          <div className="C231">
+            <p className="T098">상담 내용</p>
           </div>
           <div className="C232"></div>
         </div>
       </PopupSectionBox>
-      <PopupSectionBox x={390} y={710} width={520} height={260}>
-        <div className="C230"></div>
-      </PopupSectionBox>
-      <PopupSectionBox x={390} y={990} width={520} height={140}>
-        <div className="C230"></div>
-      </PopupSectionBox>
-      <PopupSectionBox x={930} y={190} width={600} height={240}>
-        <div className="C230"></div>
-      </PopupSectionBox>
       <PopupSectionBox x={930} y={450} width={600} height={260}>
-        <div className="C230"></div>
+        <div className="C233">
+          <div className="C231">
+            <p className="T098">기수 선택</p>
+          </div>
+        </div>
       </PopupSectionBox>
       <PopupSectionBox x={930} y={570} width={600} height={560}>
-        <div className="C230"></div>
+        <div className="C233">
+          <div className="C231">
+            <p className="T098">패키지 내역</p>
+          </div>
+          <div className="C232"></div>
+        </div>
       </PopupSectionBox>
 
       {/* 고객 기본 정보 중첩 팝업 */}
